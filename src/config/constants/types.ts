967f1ff5f0149875a9c6906ea0b8c1dd72cea1d7
mafiahod <44860990+mafiahod@users.noreply.github.com>
2021-07-01 17:31:48 +0700

    farm

---
+++ src/config/constants/types.ts
@@ -0,0 +1,27 @@
+
+export interface Address {
+    97?: string
+    56: string
+  }
+  export interface Token {
+    symbol: string
+    address?: Address
+    decimals?: number
+    projectLink?: string
+    busdPrice?: string
+  }
+  
+export interface FarmConfig {
+    pid: number
+    lpSymbol: string
+    lpAddresses: Address
+    token: Token
+    quoteToken: Token
+    multiplier?: string
+    isCommunity?: boolean
+    dual?: {
+      rewardPerBlock: number
+      earnLabel: string
+      endBlock: number
+    }
+  }
